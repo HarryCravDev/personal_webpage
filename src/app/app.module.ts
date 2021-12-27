2796e14fc97e46fa79cac254a2d18e5558ed8977
Harry Craven <harrycraven.dev@gmail.com>
2021-12-27 14:43:10 +0000

    Adding font awesome CDN & HttpClientModule for from requests.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -18,7 +19,7 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     ProjectCardComponent,
     ContactFormComponent,
   ],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
