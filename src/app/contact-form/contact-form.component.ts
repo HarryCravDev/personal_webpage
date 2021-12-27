5a06f248648af323f37472493fd4a3ee8b2c154e
Harry Craven <harrycraven.dev@gmail.com>
2021-12-27 16:56:49 +0000

    Adding contact form component files and service.

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ContactFormService } from '../services/contact-form.service';
 
 @Component({
   selector: 'app-contact-form',
@@ -7,11 +8,35 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./contact-form.component.css'],
 })
 export class ContactFormComponent implements OnInit {
-  constructor() {}
+  messageSuccess: boolean = false;
+  emailText: string = '';
+  subjectText: string = '';
+  textAreaText: string = '';
+  successInterval: any;
+
+  constructor(private contactFormService: ContactFormService) {}
 
   ngOnInit(): void {}
 
-  onSubmit(form: NgForm) {
-    console.log('Form: ', form);
+  async onSubmit(form: NgForm) {
+    const res = await this.contactFormService.postMessage(form.value);
+
+    res.subscribe((message) => {
+      this.successInterval = setInterval(() => {
+        this.messageSuccess = false;
+        clearInterval(this.successInterval);
+      }, 3000);
+
+      // Show text alert informing message was corrently send
+      this.messageSuccess = true;
+    });
+
+    this.clearInputFields();
+  }
+
+  clearInputFields() {
+    this.emailText = '';
+    this.subjectText = '';
+    this.textAreaText = '';
   }
 }
---
+++ src/app/services/contact-form.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ContactMessage } from '../contact-form/models/contact.message.model';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json',
+  }),
+};
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ContactFormService {
+  constructor(private http: HttpClient) {}
+
+  async postMessage(message: ContactMessage): Promise<Observable<any>> {
+    console.log('Message: ', { message });
+
+    const res = await this.http.post<any>(
+      'http://localhost:3005/message',
+      message,
+      httpOptions
+    );
+
+    console.log('Service: message ', { res });
+
+    return res;
+  }
+}
