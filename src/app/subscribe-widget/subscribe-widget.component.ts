8db744a292f875c4fdf8f2a867b89279fa3b719e
Harry Craven <harrycraven.dev@gmail.com>
2021-12-30 15:45:34 +0000

    Adding form to component and inputs for future development.

--- src/app/subscribe-widget/subscribe-widget.component.ts
+++ src/app/subscribe-widget/subscribe-widget.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { DataRequestService } from '../services/data-request.service';
 
 @Component({
   selector: 'app-subscribe-widget',
   templateUrl: './subscribe-widget.component.html',
-  styleUrls: ['./subscribe-widget.component.css']
+  styleUrls: ['./subscribe-widget.component.css'],
 })
 export class SubscribeWidgetComponent implements OnInit {
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() inputLabel: string | null = null;
+  @Input() inputType: string = 'text';
+  @Input() buttonText: string = 'Submit';
+  @Input() imageUrl: string = '';
+  emailText: string = '';
 
-  constructor() { }
+  constructor(private dataRequest: DataRequestService) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onSubmit(form: NgForm) {
+    this.dataRequest.postSubscribe(form.value.email);
+    this.emailText = '';
   }
-
 }
