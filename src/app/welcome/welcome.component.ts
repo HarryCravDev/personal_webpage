42a1ce41fe46c7bb2a814b68a65b76a95cfbbcdd
Harry Craven <harrycraven.dev@gmail.com>
2022-01-03 12:27:30 +0000

    Adding welcome screen typing effect.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -3,13 +3,34 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
+  index: number = 0;
+  speed: number = 140;
+  titleTemplate: string = 'Hello there';
+  title: string = '';
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.startTypeWriter();
   }
 
+  startTypeWriter() {
+    const foo = setInterval(() => {
+      if (this.title.length === this.titleTemplate.length) {
+        clearInterval(foo);
+      }
+      this.typeWriter();
+    }, this.speed);
+  }
+
+  typeWriter(type?: string) {
+    if (this.index < this.titleTemplate.length) {
+      console.log('Type...');
+      this.title += this.titleTemplate.charAt(this.index);
+      this.index++;
+    }
+  }
 }
