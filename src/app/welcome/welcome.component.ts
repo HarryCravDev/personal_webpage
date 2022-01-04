0b7f7e124098415a48784d2a3d51cf1a009a0181
Harry Craven <harrycraven.dev@gmail.com>
2022-01-04 16:03:40 +0000

    Adding typing animation effect to title and paragraph. Refactor required.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css'],
 })
 export class WelcomeComponent implements OnInit {
-  index: number = 0;
-  speed: number = 140;
+  indexTitle: number = 0;
+  indexPara: number = 0;
+  speed: number = 130;
   titleTemplate: string = 'Hello there';
   title: string = '';
+  paraTemplate: string =
+    'I am a Full Stack Developer with a huge passion for all things tech';
+  para: string = '';
 
   constructor() {}
 
@@ -18,19 +22,36 @@ export class WelcomeComponent implements OnInit {
   }
 
   startTypeWriter() {
-    const foo = setInterval(() => {
+    const titleInterval = setInterval(() => {
       if (this.title.length === this.titleTemplate.length) {
-        clearInterval(foo);
+        clearInterval(titleInterval);
       }
-      this.typeWriter();
+      this.typeWriter('title');
     }, this.speed);
+
+    const paraInterval = setInterval(() => {
+      if (this.para.length === this.paraTemplate.length) {
+        clearInterval(paraInterval);
+      }
+      this.typeWriter('para');
+    }, 100);
   }
 
-  typeWriter(type?: string) {
-    if (this.index < this.titleTemplate.length) {
-      console.log('Type...');
-      this.title += this.titleTemplate.charAt(this.index);
-      this.index++;
+  typeWriter(type: string) {
+    if (type === 'title') {
+      if (this.indexTitle < this.titleTemplate.length) {
+        console.log('Type Title...');
+        this.title += this.titleTemplate.charAt(this.indexTitle);
+        this.indexTitle++;
+      }
+    }
+
+    if (type === 'para') {
+      if (this.indexPara < this.paraTemplate.length) {
+        console.log('Type Para...');
+        this.para += this.paraTemplate.charAt(this.indexPara);
+        this.indexPara++;
+      }
     }
   }
 }
