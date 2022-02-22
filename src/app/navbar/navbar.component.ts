b3c6f3ae81d94d2386b3bd90a2416084ebe9a8c4
Harry Craven <harrycraven.dev@gmail.com>
2022-02-22 17:50:01 +0000

    Updated daisyui, added drawer for mobile navbar.

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  @Output() expandMenu = new EventEmitter();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onExpandMenu() {
+    this.expandMenu.emit();
   }
-
 }
