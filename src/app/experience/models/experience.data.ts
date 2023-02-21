85c293e8db809b62755491d8b855a69023dec691
Harry Craven <harrycraven.dev@gmail.com>
2023-02-21 21:15:14 +0000

    Updating app data.

--- src/app/experience/models/experience.data.ts
+++ src/app/experience/models/experience.data.ts
@@ -3,22 +3,25 @@ import { Experience } from './experience.model';
 export const experience: Experience[] = [
   {
     company: 'IMS Evolve',
-    jobTitle: 'Junior Full Stack Developer',
-    description: 'Developing new web applications and maintaining features all across the stack for a number of high profile clients. Made use of the latest web technology from Angular for UIs and TypeScript APIs for backend development.',
+    jobTitle: 'Full Stack Developer',
+    description:
+      'Developing new web applications and maintaining features all across the stack for a number of high profile clients. Made use of the latest web technology from Angular for UIs and TypeScript APIs for backend development.',
     fromDate: '01/12/2020',
     toDate: 'Present',
   },
   {
     company: 'Total System Services',
     jobTitle: 'Test Analysis',
-    description: 'Testing web applications using manual and automated testing techniques end-to-end, documenting defects in an understandable and organised way.',
+    description:
+      'Testing web applications using manual and automated testing techniques end-to-end, documenting defects in an understandable and organised way.',
     fromDate: '01/05/2019',
     toDate: '01/12/2020',
   },
   {
     company: 'Teesside University, Middlesbrough',
     jobTitle: 'Game Development',
-    description: 'In studying game development I built and learnt the principles of working and managing large projects that needed to be completed within a set deadline.',
+    description:
+      'In studying game development I built and learnt the principles of working and managing large projects that needed to be completed within a set deadline.',
     fromDate: '25/09/2014',
     toDate: '28/05/2017',
   },
