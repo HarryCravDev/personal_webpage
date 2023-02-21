5462f52b7b2fa4ae24e2c347c6e63d6c78fdaf73
Harry Craven <harrycraven.dev@gmail.com>
2023-02-21 17:57:09 +0000

    Removed package and updated config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,35 @@ module.exports = {
     enabled: guessProductionMode(),
     content: ["./src/**/*.{html,ts}"],
   },
-  darkMode: "class", // or 'media' or 'class'
-  theme: {
-    extend: {},
+  darkMode: false, // or 'media' or 'class'
+  daisyui: {
+    // themes: ["dark"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#793df9",
+
+          secondary: "#D926A9",
+
+          accent: "#1FB2A6",
+
+          neutral: "#191D24",
+
+          "base-100": "#2A303C",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
   },
+
+  // themes: false,
   variants: {
     extend: {},
   },
