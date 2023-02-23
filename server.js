c4d5d51aedb8c23e883f9ee07e55be777ae1dc61
Harry Craven <harrycraven.dev@gmail.com>
2023-02-23 17:23:20 +0000

    Resetting server.js

--- server.js
+++ server.js
@@ -2,19 +2,18 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist/personal-website"));
+function init() {
+  try {
+    app.use(express.static(__dirname + "/dist/personal-website"));
 
-// Redirect HTTP to HTTPS
-app.use((req, res, next) => {
-  if (req.protocol === "http") {
-    console.log("Redirecting to https://" + req.headers.host + req.url);
-    res.redirect("https://" + req.headers.host + req.url);
-  } else {
-    next();
+    app.get("/*", function (req, res) {
+      res.sendFile(path.join(__dirname + "/dist/personal-website/index.html"));
+    });
+
+    app.listen(process.env.PORT || 8080);
+  } catch (error) {
+    console.log("Error: ", error);
   }
-});
+}
 
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/personal-website/index.html"));
-});
-app.listen(process.env.PORT || 8080);
+init();
